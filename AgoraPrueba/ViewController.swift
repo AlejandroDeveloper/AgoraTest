19344645abbfce0cd8e226d053f6127e79941e1c
AlejandroDeveloper <alejandro.palomo@external.grifols.com>
2018-02-20 14:40:10 +0100

    Initial Commit

---
+++ AgoraPrueba/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AgoraPrueba
+//
+//  Created by Alejandro Palomo Rodriguez on 20/2/18.
+//  Copyright © 2018 Alejandro Palomo Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
